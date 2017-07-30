dd4ebf70692226eadfa7ffabc9f48e842526c1a2
Chris Richardson <chris@chrisrichardson.net>
2017-07-30 16:27:54 -0700

    Commit for lab 01

--- open-web-pages.sh
+++ open-web-pages.sh
@@ -2,7 +2,7 @@
 
 HOST=${1?}
 
-open http://${DOCKER_HOST_IP?}:8761/
+open http://${HOST?}:8761/
 open http://${HOST}:8081/swagger-ui.html
 open http://${HOST}:8082/swagger-ui.html
 open http://${HOST}:8083/swagger-ui.html

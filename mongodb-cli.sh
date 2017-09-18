c1698750a48342331599371651606b330b8e2982
Chris Richardson <chris@chrisrichardson.net>
2017-09-17 17:07:49 -0700

    Commit for complete

--- mongodb-cli.sh
+++ mongodb-cli.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm --link $(echo ${PWD##*/} | sed -e 's/-//g')_mongodb_1:mongodb -i -t mongo:3.0.4 /usr/bin/mongo --host mongodb bankingexampledb
+docker run --link $(echo ${PWD##*/} | sed -e 's/-//g')_mongodb_1:mongodb -i -t --rm mongo:3.0.4 sh -c "exec /usr/bin/mongo --host mongodb bankingexampledb"

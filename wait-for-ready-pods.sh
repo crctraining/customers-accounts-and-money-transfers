7ea6ff47b82ff924dda206ffedf81476a3a4881d
Chris Richardson <chris@chrisrichardson.net>
2019-01-13 10:45:36 -0800

    Commit for lab 08

---
+++ wait-for-ready-pods.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+services="customer-service account-group-service  account-service customer-view-service money-transfer-service"
+
+done=false
+
+# ?(@.name =~ /.*-service/)
+
+while [[ "$done" = false ]]; do
+    for service in $services ; do
+            ready=$(kubectl get po -l svc=$service -o "jsonpath={.items[0].status.containerStatuses[?(@.name == \"$service\" )].ready}")
+            echo $service ready=$ready
+            if [[ "$ready" = "true" ]]; then
+                 echo $service is ready
+                 done=true
+            else
+                 echo $service is NOT ready
+                 done=false
+                 break
+            fi
+    done
+    if [[ "$done" = true ]]; then
+            break;
+  fi
+  echo -n .
+  sleep 1
+done
